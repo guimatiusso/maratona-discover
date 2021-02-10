18cd2a419800f323a9ed95b1d39b9572cc369d66
guimatiusso <rottamatiusso@gmail.com>
2021-02-10 17:06:18 -0300

    mais algumas modificaçoes minimas feitas no projeto

--- index.php
+++ index.php
@@ -19,88 +19,108 @@
   <body>
     <div class="container-fluid header">
         <div class="row justify-content-md-center mb-5">
-            <div class="col-md-auto">
+            <div class="col-auto">
                 <img src="./images/logo.svg" class="img-fluid" alt="Logo Dev Finance">
             </div>
         </div>
     </div>
 
     <main>
-        <div>
-            <div class="container balanco">
-                <div class="row">
-                    <h2 class="mb-3 sr-only">Balanço</h2>
+        <div class="container balanco">
+            <h2 class="mb-3 sr-only">Balanço</h2>
+            <div class="card py-4 px-5 mb-4 mx-5">
+                <h3 class="fs-6 text">
+                    <span>Entradas</span>
+                    <img src="./images/income.svg" alt="Imagem de Entradas">
+                </h3>
+                <p class="fs-4 text lh-base mt-3" id="incomeDisplay">R$ 5.000,00</p>
+            </div>
+            <div class="card py-4 px-5 mb-4 mx-5">
+                <h3 class="fs-6 text">
+                    <span>Saídas</span>
+                    <img src="./images/expense.svg" alt="Imagem de Saidas">
+                </h3>
+                <p class="fs-4 text lh-base mt-3" id="expenseDisplay">R$ 2.000,00</p>
+            </div>
+            <div class="card total py-4 px-5 mb-4 mx-5">
+                <h3 class="fs-6 text">
+                    <span>Total</span>
+                    <img src="./images/total.svg" alt="Imagem de Total">
+                </h3>
+                <p class="fs-4 text lh-base mt-3" id="totalDisplay">R$ 3.000,00</p>
+            </div>
+        </div>
 
-                    <div class="card py-4 px-5 mb-4 mx-5" style="width: auto;">
-                        <h3 class="fs-6 text">
-                            <span>Entradas</span>
-                            <img src="./images/income.svg" alt="Imagem de Entradas">
-                        </h3>
-                        <p class="fs-2 text lh-lg mt-3">R$ 5.000,00</p>
-                    </div>
-                    <div class="card py-4 px-5 mb-4 mx-5" style="width: auto;">
-                        <h3 class="fs-6 text">
-                            <span>Saídas</span>
-                            <img src="./images/expense.svg" alt="Imagem de Saidas">
-                        </h3>
-                        <p class="fs-2 text lh-lg mt-3">R$ 2.000,00</p>
-                    </div>
-                    <div class="card total py-4 px-5 mb-4 mx-5" style="width: auto;">
-                        <h3 class="fs-6 text">
-                            <span>Total</span>
-                            <img src="./images/total.svg" alt="Imagem de Total">
-                        </h3>
-                        <p class="fs-2 text lh-lg mt-3">R$ 3.000,00</p>
-                    </div>
+        <div class="container transacoes">
+            <h2 class="mt-5 mb-3 fw-normal sr-only">Transações</h2>
+
+            <!-- Button trigger modal -->
+            <!-- onclick="modal.open()" -->
+            <a type="button" class="btn btn-link text-decoration-none mb-3" data-bs-toggle="modal" data-bs-target="#exampleModal">+ Nova Transação</a>
+
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th scope="col" class="py-3 px-4 border-0">Descrição</th>
+                        <th scope="col" class="py-3 px-4 border-0">Valor</th>
+                        <th scope="col" class="py-3 px-4 border-0">Data</th>
+                        <th scope="col" class="py-3 px-4 border-0"></th>
+                    </tr>
+                </thead>
+                <tbody>
+                </tbody>
+            </table>
+        </div>
+    </main>
+
+    <!-- Modal -->
+    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="adicionarNovaTrasação" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered">
+            <div class="modal-content">
+                <div class="modal-body">
+                    <h6>Nova Transação</h6>
+                    <form action="" onsubmit="Form.submit(event)">
+                        <div class="mb-3">
+                            <label for="descricao" class="form-label sr-only">Descrição</label>
+                            <input 
+                                type="text" 
+                                class="form-control" 
+                                id="descricao" 
+                                name="descricao" 
+                                aria-describedby="Descrição da transação" 
+                                placeholder="Descrição">
+                        </div>
+                        <div class="mb-3">
+                            <label for="valor" class="form-label sr-only">Valor</label>
+                            <input 
+                                type="number" 
+                                class="form-control" 
+                                id="valor" 
+                                name="valor" 
+                                aria-describedby="Valor da transação" 
+                                placeholder="0,00" 
+                                step="0.01">
+                            <div id="valorlHelp" class="form-text">Use o sinal - (negativo) para despesas e , (vírgula) para casas decimais.</div>
+                        </div>
+                        <div class="mb-3">
+                            <label for="data" class="form-label sr-only">Data</label>
+                            <input 
+                                type="date" 
+                                class="form-control" 
+                                id="data" 
+                                name="data" 
+                                aria-describedby="Data da transação">
+                        </div>
+                        <div class="modal-footer justify-content-between">
+                            <!-- onclick="modal.close()" -->
+                            <a href="#" class="btn btn-link px-5 py-3 text-decoration-none shadow-none border border-2 border-danger" data-bs-dismiss="modal">Cancelar</a>
+                            <button type="submit" class="btn btn-primary px-5 py-3 border-0 shadow-none">Submit</button>
+                        </div>
+                    </form>
                 </div>
             </div>
         </div>
-
-        <div>
-            <div class="container transacoes">
-                <h2 class="mt-5 mb-3 fw-normal sr-only">Transações</h2>
-
-                <a href="#" class="btn btn-link text-decoration-none mb-3">+ Nova Transação</a>
-
-                <table class="table">
-                    <thead>
-                        <tr>
-                            <th scope="col" class="border-bottom py-3 px-4">Descrição</th>
-                            <th scope="col" class="border-bottom py-3 px-4">Valor</th>
-                            <th scope="col" class="border-bottom py-3 px-4">Data</th>
-                            <th scope="col" class="border-bottom py-3 px-4"></th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <td class="descricao">Luz</td>
-                            <td class="negativo">- R$ 500,00</td>
-                            <td class="data">23/01/2021</td>
-                            <td>
-                                <img src="./images/minus.svg" alt="Remover transação">
-                            </td>
-                        </tr>
-                        <tr>
-                            <td class="descricao">Criação de Website</td>
-                            <td class="positivo">R$ 5.000,00</td>
-                            <td class="data">24/01/2021</td>
-                            <td>
-                                <img src="./images/minus.svg" alt="Remover transação">
-                            </td>
-                        </tr>
-                        <tr>
-                            <td class="descricao">Aluguel</td>
-                            <td class="negativo">- R$ 1.500,00</td>
-                            <td class="data">26/01/2021</td>
-                            <td>
-                                <img src="./images/minus.svg" alt="Remover transação">
-                            </td>
-                        </tr>
-                    </tbody>
-                </table>
-            </div>
-        </div>
-    </main>
+    </div>
 
     <footer class="text-center">
         <div class="container footer pt-5 pb-4">
@@ -112,6 +132,8 @@
 
     <!-- Option 1: Bootstrap Bundle with Popper -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
+
+    <script src="./scripts.js"></script>
 
     <!-- Option 2: Separate Popper and Bootstrap JS -->
     <!--
